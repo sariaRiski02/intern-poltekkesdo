2aef3ba530e59ab307e7713b25b2cb1b8ede5406
sariaRiski02 <muhammatsaria026@student.unsrat.ac.id>
2025-09-12 13:01:15 +0800

    fixed: entry dummy data into database

--- database/migrations/2025_09_09_055754_create_admins_table.php
+++ database/migrations/2025_09_09_055754_create_admins_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('fullname')->nullable(true);
             $table->string('nip')->nullable(true);
-            $table->foreignId('user_id')->constrained('admins')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
